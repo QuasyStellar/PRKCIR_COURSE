72c8a06581816cf4fbed36c722e611bcfc25f438
quasys <76686085+QuasyStellar@users.noreply.github.com>
2024-12-25 01:27:12 +0300

    Massive design improvements, fixed roadmap, added contacts and some minor fixes

--- js/roadmap.js
+++ js/roadmap.js
@@ -1,15 +1,56 @@
-const blockchain = document.querySelector('.blockchain');
+const hexGrid = document.querySelector('.hex-grid');
 
-let rotateX = 20;
-let rotateY = -30;
 
-document.addEventListener('mousemove', (e) => {
-    const { clientX, clientY } = e;
-    const { innerWidth, innerHeight } = window;
+        // Обработка мыши
+        roadmap.addEventListener('mousemove', (event) => {
+            const x = event.clientX;
+            const y = event.clientY;
 
-    rotateY = -30 + (clientX / innerWidth) * 60;
-    rotateX = 20 - (clientY / innerHeight) * 40;
+            const centerX = window.innerWidth / 2;
+            const centerY = window.innerHeight / 2;
 
-    blockchain.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+            const deltaX = (x - centerX) / centerX;
+            const deltaY = (y - centerY) / centerY;
+
+            // Увеличиваем множители для большего поворота
+            hexGrid.style.transform = `rotateX(${-deltaY * 30}deg) rotateY(${deltaX * 30}deg)`;
+        });
+
+        // Обработка сенсорных касаний
+        roadmap.addEventListener('touchstart', (event) => {
+            isTouching = true;
+            touchStartX = event.touches[0].clientX;
+            touchStartY = event.touches[0].clientY;
+        });
+
+        roadmap.addEventListener('touchmove', (event) => {
+            if (!isTouching) return;
+
+            const x = event.touches[0].clientX;
+            const y = event.touches[0].clientY;
+
+            const deltaX = (x - touchStartX) / window.innerWidth;
+            const deltaY = (y - touchStartY) / window.innerHeight;
+
+            // Увеличиваем множители для большего поворота
+            hexGrid.style.transform = `rotateX(${-deltaY * 30}deg) rotateY(${deltaX * 30}deg)`;
+
+            // Обновление начальных координат для следующего касания
+            touchStartX = x;
+            touchStartY = y;
+        });
+
+        roadmap.addEventListener('touchend', () => {
+            isTouching = false;
+        });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const menu = document.querySelector('.menu');
+
+    menuToggle.addEventListener('click', function() {
+        menu.classList.toggle('open'); // Добавляем или убираем класс 'open'
+    });
 });
 
